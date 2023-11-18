f261491829a3f7c378b4c2ee93d3c18efbe77e23
Saurabh Patel <999.sbpatel@gmail.com>
2023-11-18 16:10:48 +0530

    Connection features are extracted , now working on Host feature

--- developing_script.py
+++ developing_script.py
@@ -19,6 +19,8 @@ class ConnectionRecord:
         self.urgent = 0
         self.timestamp = packet_list[-1].sniff_timestamp
         self.duration = 0
+        self.dst_ip = None
+        self.src_ip = None
 
     def process(self, service_mapping):
         if "tcp" in self.packet_list[0]:
@@ -93,18 +95,18 @@ class ConnectionRecord:
         self.protocol = 'icmp'
         self.src_port = int(self.packet_list[0].icmp.srcport)
         self.dst_port = int(self.packet_list[0].icmp.dstport)
-        self.duration = float(self.packet_list[0].icmp.time_relative)
+        self.duration = float(self.packet_list[-1].icmp.time_relative)
     
         self.service = 'eco_i'         # ? why only eco_i , lets serch for other 
         pass
 
-    def _process_bytes_land_wrong_urgent_timestamp_duration(self):
+    def _process_bytes_land_wrong_urgent_timestamp(self):
         if self.src_ip == self.dst_ip and self.src_port == self.dst_port:
             land = 1
         else:
             land = 0
 
-        timestamp = self.packet_list[-1].sniff_timestamp
+        self.timestamp = self.packet_list[-1].sniff_timestamp
         # traverse packets (some basic features are aggregated from each packet in whole connection)
         for packet in self.packet_list:
             if 'ip' in self.packet_list[0]:
@@ -132,17 +134,84 @@ class ConnectionRecord:
             elif self.protocol == 'icmp':
                 if packet.icmp.checksum_status != '2':
                     self.wrong_frag += 1
-        self.timestamp = self.packet_list[-1].sniff_timestamp
+        
         pass
 
-    def _process_status_flag(self):
+    def get_connection_status(packets, ipv4=True):
         
+
+        def process_packet_key(packet, source_ip):
+            if ipv4:
+                if source_ip == packet.ip.src:
+                    return ('1', packet.tcp.flags_syn, packet.tcp.flags_ack, packet.tcp.flags_reset, packet.tcp.flags_fin)
+                else:
+                    return ('0', packet.tcp.flags_syn, packet.tcp.flags_ack, packet.tcp.flags_reset, packet.tcp.flags_fin)
+            else:
+                if source_ip == packet.ipv6.src:
+                    return ('1', packet.tcp.flags_syn, packet.tcp.flags_ack, packet.tcp.flags_reset, packet.tcp.flags_fin)
+                else:
+                    return ('0', packet.tcp.flags_syn, packet.tcp.flags_ack, packet.tcp.flags_reset, packet.tcp.flags_fin)
+
+        # Connection status mapping
+        conn = {
+            'INIT': {('0', '1', '1', '0', '0'): 'S4', ('1', '0', '0', '0', '1'): 'SH', ('1', '1', '0', '0', '0'): 'S0'},
+            'S4': {('0', '0', '0', '1', '0'): 'SHR', ('0', '0', '0', '0', '1'): 'RSTRH'},
+            'SH': {}, 'SHR': {}, 'RSTRH': {}, 'OTH': {}, 'S0': {('0', '1', '1', '0', '0'): 'S1', ('0', '0', '0', '1', '0'): 'REJ', ('1', '0', '0', '1', '0'): 'RST0S0'},
+            'REJ': {}, 'RST0S0': {}, 'RST0': {}, 'RSTR': {}, 'S1': {('1', '0', '1', '0', '0'): 'ESTAB', ('1', '0', '0', '1', '0'): 'RST0', ('0', '0', '0', '1', '0'): 'RSTR'},
+            'ESTAB': {('1', '0', '1', '0', '1'): 'S2', ('0', '0', '1', '0', '1'): 'S3'},
+            'S2': {('0', '0', '1', '0', '0'): 'SF'},
+            'S3': {('1', '0', '1', '0', '0'): 'SF'},
+            'SF': {}
+        }
+
+        # Define source IP
+        if ipv4:
+            source_ip = packets[0].ip.src
+        else:
+            source_ip = packets[0].ipv6.src
+
+        # Initialize connection status
+        connection_status = 'INIT'
+
+        # Process each packet and update connection status
+        for packet in packets:
+            key = process_packet_key(packet, source_ip)
+            try:
+                connection_status = conn[connection_status][key]
+            except KeyError:
+                status_mapping = {
+                    'INIT': 'OTH',
+                    'SH': 'SH',
+                    'SHR': 'SHR',
+                    'RSTRH': 'RSTRH',
+                    'OTH': 'OTH',
+                    'REJ': 'REJ',
+                    'RST0S0': 'RST0S0',
+                    'RST0': 'RST0',
+                    'RSTR': 'RSTR',
+                    'SF': 'SF'
+                }
+                return status_mapping.get(connection_status, 'OTH')
+
+        return connection_status
+
+
+
+    def _process_status_flag_IP(self):
+        if 'ip' in self.packet_list[0]:
+            self.src_ip = self.packet_list[0].ip.src
+            self.dst_ip = self.packet_list[0].ip.dst
+            self.status_flag = self.get_connection_status(self.packet_list)
+        else:
+            self.src_ip = self.packet_list[0].ipv6.src
+            self.dst_ip = self.packet_list[0].ipv6.dst
+            self.status_flag = self.get_connection_status(self.packet_list, False)
         pass
 
     def __str__(self):
         return (
-            f"{self.timestamp},{self.packet_list[0].ip.src},{self.src_port},"
-            f"{self.packet_list[0].ip.dst},{self.dst_port},{self.idx},"
+            f"{self.timestamp},{self.src_ip},{self.src_port},"
+            f"{self.dst_ip},{self.dst_port},{self.idx},"
             f"{self.duration},{self.protocol_type},{self.service},{self.status_flag},"
             f"{self.src_bytes},{self.dst_bytes},{self.land},{self.wrong_fragment},{self.urgent}"
         )
@@ -158,7 +227,6 @@ class NetworkPacketSniffer:
                 "src_port",
                 "dst_ip",
                 "dst_port",
-                "index",
                 "idx",
                 "duration",
                 "protocol_type",
@@ -261,3 +329,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
